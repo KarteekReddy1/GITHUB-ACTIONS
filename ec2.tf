1e661761b0d56333257a13a3b9771534d56e8413
KarteekReddy1 <104560543+KarteekReddy1@users.noreply.github.com>
2026-03-27 21:35:36 +0530

    Add Terraform configuration for AWS EC2 setup
    Define AWS provider, VPC, subnet, security group, and EC2 instance.

---
+++ ec2.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "us-east-1"
+}
+data "aws_vpc" "default" {
+  default = true
+}
+data "aws_subnet" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+resource "aws_security_group" "sg" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Demo only, restrict in production
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-02dfbd4ff395f2a1b" # Amazon Linux 2 (us-east-1)
+  instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnet.default.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
+
+
+
+
+
+
+
