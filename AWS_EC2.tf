2cc261f8e477abe893a03acb9862c81db1f7c27e
KarteekReddy1 <104560543+KarteekReddy1@users.noreply.github.com>
2026-04-07 21:47:00 +0530

    Replace custom security group with default security group

--- AWS_EC2.tf
+++ AWS_EC2.tf
@@ -1,48 +1,34 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+# Get the default VPC
 data "aws_vpc" "default" {
   default = true
 }
+
+# Get a default subnet from that VPC
 data "aws_subnet" "default" {
   vpc_id = data.aws_vpc.default.id
 }
-resource "aws_security_group" "sg_5" {
-  name        = "allow_ssh_5"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = data.aws_vpc.default.id
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Demo only, restrict in production
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+# Get the default security group for the default VPC
+data "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "group-name"
+    values = ["default"]
   }
 }
 
+# Launch EC2 instance using the default SG
 resource "aws_instance" "example" {
   ami                    = "ami-02dfbd4ff395f2a1b" # Amazon Linux 2 (us-east-1)
   instance_type          = "t2.micro"
   subnet_id              = data.aws_subnet.default.id
-  vpc_security_group_ids = [aws_security_group.sg_5.id]
+  vpc_security_group_ids = [data.aws_security_group.default.id]
 
   tags = {
     Name = "Terraform-EC2"
   }
 }
-
-
-
-
-
-
-
-
